c447a925aefb7d9a42e3ba4bc4730f606cde8ad4
andychong1996 <andygg1996personal@gmail.com>
2015-11-03 15:50:11 +0800

    Added skeleton, users table

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get craete" do
+    get :craete
+    assert_response :success
+  end
+
+  test "should get add" do
+    get :add
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get :destroy
+    assert_response :success
+  end
+
+end
